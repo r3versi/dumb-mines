db105d635722e5645a0413ec5cb65b42d25d5ca0
Raffaele Campanile <campanile.ra@gmail.com>
2018-11-03 04:50:47 -0700

    Enhancing

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Dumb Mines
 ### The most exciting minesweeper version on your terminal
 
-This repository contains a version of the game *the minesweeper*.<br/>
+This repository contains a version of the game *the minesweeper* playable on the command line.<br/>
+<!--
 Some lessons at University were really boring that we started playing the version installed by default on our linux version.<br/>
 Once we mastered it, we came up with the idea of developing our own version of the game.<br/>
-The project was thought to be the fisrt step of a bigger that we wanted to carry on setting up a graphic user interface and then training an AI able to win efficiently the board.<br/>
+-->
+The project was thought to be the fisrt step of a bigger one, that we wanted to carry on setting up a graphic user interface and then training an AI able to win efficiently the board.<br/>
 Unfortunately we are stuck at the first step (you know, exams at University, other projects going on...).<br/>
-If you want to contribuite with some new stuff feel totally free to contact us or to fork something good.<br/>
-If you just want to play the game instead, clone or download this repository and run **python game.py** on your command line.<br/>
+Waiting for updates, you can just play the game yourself: clone or download this repository and run **python game.py** on your command line.<br/>
 Enjoy getting blown :3 <br/>
 
 <!--
--- game.py
+++ game.py
@@ -2,11 +2,8 @@ import numpy as np
 from numpy.random import randint
 from scipy.signal import convolve2d
 
-############################################################################
-# This class contains the rules to define propely a Board containing bombs #
-############################################################################
-
 class Board:
+	'''Contains the rules to define propely a board containing bombs.'''
 	def __init__(self,width=10,height=10,n_bombs=10):
 		self.width = width
 		self.height = height
@@ -17,8 +14,8 @@ class Board:
 		self.n_bombs = n_bombs
 		self.shape = (width, height)
 
-	# This method randomly place the bombs on the board
 	def generate(self):
+		'''Randomly places the bombs on the board.'''
 		self.bombs = np.zeros(shape = (self.width, self.height), dtype=np.int8)
 		
 		placed = 0
@@ -34,32 +31,33 @@ class Board:
 
 		self.compute_counts()
 
-	# This method employs 2d convolution to find the number of bombs nearby a dowel
 	def compute_counts(self):
+		'''Employs 2d convolution to find the number of bombs adjacent to a box.'''
 		ker = np.array([[1,1,1],[1,0,1],[1,1,1]])
 		self.counts = convolve2d(self.bombs, ker, 'same')
 
 	def is_bomb(self,x,y):
+		'''Reveals if the box hide a bomb.'''
 		return self.bombs[x,y] == 1
 
 	def is_empty(self,x,y):
+		'''Reveals if the box is empty.'''
 		return self.counts[x,y] == 0
 
 	def inside(self,x,y):
+		'''Returns True or Flase depending on whether the coordinates (x,y) fall inside of outside the board.'''
 		return (x >= 0) & (y >= 0) & (x < self.width) & (y < self.height)
 	
 	def load(self,file):
 		pass
 
-####################################################
-# This class contains the rules to set up the game #
-####################################################
 class Game:
+	'''Contains the rules to set up the game.'''
 	def __init__(self):
 		pass
 
-	# This method start the games reading external input the features of the board
 	def start(self):
+		'''Start the game reading from the standard input the features of the board.'''
 		print('#'*20)
 		print("## Mines of doom v0.1\n")
 
@@ -80,7 +78,7 @@ class Game:
 		self.revealed = np.zeros(shape = self.board.shape)
 	
 	def print_board(self):
-
+		'''Print the board on the standard output.'''
 		for x in range(self.board.width):
 			for y in range(self.board.height):
 				if self.revealed[x,y]:
@@ -92,10 +90,8 @@ class Game:
 					print('X', end=' ')
 			print()
 
-	# This is the core method: it runs until you win or loose the game
-	# It ask for your next move and then compute the new board
 	def run(self):
-		
+		'''It asks for your next move and then compute the new board until you win or loose the game.'''		
 		print('#'*20)
 		self.print_board()
 
@@ -128,7 +124,7 @@ class Game:
 		return self.alive
 	
 	def expand_from(self, x, y):
-		
+		'''If the box does not hide a bomb, reaveal it and calculate how many adjacent boxes contains bombs. If no mines are adjacent, all adjacent squares are recursively revealed.'''
 		if (self.revealed[x,y] == 1) | (self.board.is_bomb(x,y)):
 			return
 		
@@ -142,11 +138,13 @@ class Game:
 					self.expand_from(x+dx,y+dy)
 
 	def game_over(self):
+		'''Set the game over screen.'''
 		print('#'*20)
 		self.print_board()
 		print('Enjoy getting blown :3')
 
 	def victory(self):
+		'''Set the victory screen.'''
 		print('#'*20)
 		self.print_board()
 		print('Congrats!!! You won the game, you are not so dumb as I thought...')
